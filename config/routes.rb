f5c51df72aa2e39cb75a2955a5a7e43ee9dad29e
mylaptop <tuananhhedspibk1@gmail.com>
2017-05-06 23:19:45 +0700

    Add some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+	root 'static_pages#home'
+	get '/signup', to: 'users#new'
+	post '/signup', to: 'users#create'	# named routes
+	get '/login', to: 'sessions#new'
+	post '/login', to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
+	get '/index', to: 'users#index'
+	get '/notification/:id/link_through', to: 'notifications#link_through',
+										 as: :link_through
+	get '/notifications', to: 'notifications#index'
+	resources :users do
+		member do
+			get :following, :followers
+		end
+	end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :comments,		only: [:create, :destroy]
+	resources :likes, 			only: [:create, :destroy]
+	resources :users
+	resources :microposts,		only: [:create, :destroy, :show]
+	resources :relationships,	only: [:create, :destroy]
 end
