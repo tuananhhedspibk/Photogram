71ca95017bc03deb8fc59269de340b4cfc2d7aa4
mylaptop <tuananhhedspibk1@gmail.com>
2017-05-06 23:22:19 +0700

    Add not_found method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+	def full_title page_title = ''
+		base_title = "Photogram"
+		if page_title.empty?
+			base_title
+		else
+			page_title
+		end
+	end
+
+	def get_suggest_user_id
+		if current_user != nil
+			@suggestUsersIds = User.find_by_sql(
+				"select id from users where id != #{current_user.id} 
+				and id not in (select followed_id from relationships 
+				where follower_id = #{current_user.id}) 
+				limit 3")
+		end
+	end
 end
