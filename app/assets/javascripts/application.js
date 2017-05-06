d2cd70aaf972a96bf90b92c34646b43ae6dd4861
Vũ Đức Minh <minhbk.mdv@gmail.com>
2017-05-06 09:04:00 +0000

    Minh commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,44 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+function internal_link_click (link) {
+	// Make sure this.hash has a value before overriding default behavior
+	if (link.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+
+		// Store hash
+		var hash = link.hash;
+
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 900, function(){
+		
+		// Add hash (#) to URL when done scrolling (default click behavior)
+		window.location.hash = hash;
+		});
+	} // End if
+}
+
+$(window).scroll(function (event) {
+	var scroll = $(window).scrollTop();
+	if(scroll >= 70){
+		$(".search-filter").css("top", scroll - 35);
+	}
+	else if(scroll == 0){
+		$(".search-filter").css("top", 35);
+	}
+});
+
+$(function(){
+	$(".search-box").keyup(function(){
+		$.get($(".users-search").attr("action"), $(".users-search").serialize(), null, "script");
+		return false;
+	});
+});
